77942199107d3b1b3ebbedbbd8ddcc6693e39472
Manoela Urias <maanoelaurias@gmail.com~>
2023-04-30 22:00:05 -0300

    cabeçalho mobile

---
+++ app.js
@@ -0,0 +1,14 @@
+const submenu = document.querySelector(".itens")
+const menu = document.getElementById('navbar')
+
+menu.addEventListener("click", function () {
+    if (submenu) {
+        // Alternar o estado do menu suspenso
+        if (submenu.style.display === 'flex') {
+          submenu.style.display = 'none';
+        } else {
+          submenu.style.display = 'flex';
+        }
+      }
+
+})
