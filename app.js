6cd462ad8abe8345a04dad21c0910581b7514826
Manoela Urias <maanoelaurias@gmail.com~>
2023-05-05 23:54:54 -0300

    ok

--- app.js
+++ app.js
@@ -12,3 +12,40 @@ menu.addEventListener("click", function () {
       }
 
 })
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const slideIntervalTime = 2500; // tempo de intervalo em milissegundos
+
+let slideInterval = setInterval(startSlide, slideIntervalTime);
+
+function startSlide() {
+  const progressBar = document.querySelector('.progress');
+  progressBar.style.width = '0%'; // reinicia a barra de progresso
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[currentSlide].classList.add('active');
+}
+
+function resetProgress() {
+  const progressBar = document.querySelector('.progress');
+  progressBar.style.width = '0%';
+  clearInterval(slideInterval);
+  slideInterval = setInterval(startSlide, slideIntervalTime);
+}
+
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+
+next.addEventListener('click', () => {
+  resetProgress(); // reinicia a barra de progresso
+  startSlide();
+});
+
+prev.addEventListener('click', () => {
+  resetProgress(); // reinicia a barra de progresso
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+});
+
