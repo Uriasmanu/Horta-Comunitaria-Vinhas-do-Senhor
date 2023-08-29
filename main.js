86d68ae6e1ffb0b77caa2308196364bae2dad450
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-08-29 12:11:04 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-const btn = document.getElementById("menu");
 
-btn.addEventListener("click", function(event) {
-    alert("Clicou");
+const menuOpen = document.getElementById("menu-open");
+const close = document.getElementById("close");
+const menuNavbar = document.getElementById("menu-navbar"); 
+
+const screenWidth = window.screen.width;
+
+let checkbox = document.getElementById("menu");
+checkbox.addEventListener('click', function () {
+    if(checkbox.checked) and screenWidth < 600px {
+        menuOpen.style.display = "none";
+         close.style.display = "block";
+         menuNavbar.style.display = "block";
+        
+         } else {
+            menuOpen.style.display = "block";
+            close.style.display = "none";
+            menuNavbar.style.display = "none";
+         }
 });
+
+
+
+   
+    
