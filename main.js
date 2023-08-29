9208009de77a26f4ac737bc9825f7f59854024e9
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-08-29 12:14:51 -0300

    .

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const screenWidth = window.screen.width;
 
 let checkbox = document.getElementById("menu");
 checkbox.addEventListener('click', function () {
-    if(checkbox.checked & screenWidth < 600 ) {
+    if(checkbox.checked) {
         menuOpen.style.display = "none";
          close.style.display = "block";
          menuNavbar.style.display = "block";
