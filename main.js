6686aa5f8b217c5f31f4516f5f4999a12c2aaa8e
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-08-29 13:41:25 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const screenWidth = window.screen.width;
 let checkbox = document.getElementById("menu");
 checkbox.addEventListener('click', function () {
     if(checkbox.checked) {
-        menuOpen.style.display = "none";
-         close.style.display = "block";
+        menuOpen.style.display = "block";
+         close.style.display = "none";
          menuNavbar.style.display = "block";
         
          } else {
-            menuOpen.style.display = "block";
-            close.style.display = "none";
+            menuOpen.style.display = "none";
+            close.style.display = "block";
             menuNavbar.style.display = "none";
          }
 });
